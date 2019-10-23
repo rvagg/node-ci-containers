39e47b130a793a525883b9fdba7cfa144f3843b5
Rod Vagg <rod@vagg.org>
2019-10-23 16:25:14 +1100

    rename CONTAINER_TESTS to linux_x64_container_suite

--- execute.sh
+++ execute.sh
@@ -4,20 +4,20 @@
 ##
 ## Should be executed in git clone of the nodejs/node repository. The command
 ## `shyaml` should be available (from python3-pip). The repository should have
-## a .ci.yml file and $CONTAINER_TEST should be set to a named test section
-## in that YAML file. That section should contain an "image" property that will
-## be appended to 'rvagg/node-ci-containers:' to find the Docker Hub image,
-## and an "execute" property that contains the script to run to execute the
-## build & test.
+## a .ci.yml file and $linux_x64_container_suite should be set to a named test
+## section in that YAML file. That section should contain an "image" property
+## that will be appended to 'rvagg/node-ci-containers:' to find the Docker Hub
+## image, and an "execute" property that contains the script to run to execute
+## the build & test.
 
 set -x
 set -e
 
-image_name=$(cat .ci.yml | shyaml get-value "${CONTAINER_TEST}.image")
-test_label=$(cat .ci.yml | shyaml get-value "${CONTAINER_TEST}.label" || echo $CONTAINER_TEST)
-execute_cmds=$(cat .ci.yml | shyaml get-value ${CONTAINER_TEST}.execute)
+image_name=$(cat .ci.yml | shyaml get-value "tests.${linux_x64_container_suite}.image")
+test_label=$(cat .ci.yml | shyaml get-value "tests.${linux_x64_container_suite}.label" || echo $linux_x64_container_suite)
+execute_cmds=$(cat .ci.yml | shyaml get-value "tests.${linux_x64_container_suite}.execute")
 
-echo "Running ${test_label} (${CONTAINER_TEST})..."
+echo "Running ${test_label} (${linux_x64_container_suite})..."
 
 if ! [[ "$image_name" =~ ^[a-zA-Z0-9/_-]+$ ]]; then
   echo "Bad image name: ${image_name}"
@@ -26,13 +26,13 @@ fi
 
 ccache_dir=/home/iojs/.ccache/${image_name}
 echo "Using ccache directory: ${ccache_dir}"
-mkdir -p "${ccache_dir}/${CONTAINER_TEST}_${BUILD_NUMBER}"
+mkdir -p "${ccache_dir}/${linux_x64_container_suite}_${BUILD_NUMBER}"
 if [ "$image_name" == "ubuntu1804" ]; then
   # special case for most commonly used image
   echo 'max_size = 15.0G' > ${ccache_dir}/ccache.conf
 fi
 
-echo "CCACHE_TEMPDIR=/home/iojs/.ccache/${CONTAINER_TEST}_${BUILD_NUMBER}" >> env.properties
+echo "CCACHE_TEMPDIR=/home/iojs/.ccache/${linux_x64_container_suite}_${BUILD_NUMBER}" >> env.properties
 
 # env.properties has some variables we want to make available in the build
 execute_cmds=". ./env.properties; ${execute_cmds}"
