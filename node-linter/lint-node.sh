6debb7a3ff355bdcffdf410aea7e9fc50e9439da
Rod Vagg <rod@vagg.org>
2019-10-27 17:52:47 +1100

    properly lint python & with python3

--- node-linter/lint-node.sh
+++ node-linter/lint-node.sh
@@ -1,6 +1,15 @@
-#!/bin/bash -xe
+#!/bin/bash -ex
 
-# If lint-ci fails, print all the interesting lines to the console.
-make lint-ci || { 
-  cat test-eslint.tap | grep -v '^ok\|^TAP version 13\|^1\.\.' | sed '/^\s*$/d' && 
+NODEJS_MAJOR_VERSION=$(python tools/getnodeversion.py | cut -d. -f1)
+
+# Lint with python3 in new branches
+# Refs: https://github.com/nodejs/build/issues/1631
+if [[ "$NODEJS_MAJOR_VERSION" -ge "12" ]]; then
+  make lint-py-build PYTHON=python3 || true
+  make lint-py PYTHON=python3
+fi
+
+make lint-py-build PYTHON=python2 || true
+make lint-ci PYTHON=python2 || {
+  cat test-eslint.tap | grep -v '^ok\|^TAP version 13\|^1\.\.' | sed '/^\s*$/d' &&
   exit 1; }
