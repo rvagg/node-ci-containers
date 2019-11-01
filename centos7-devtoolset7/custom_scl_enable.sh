9e4da3f68a47052d8fc00a0c6c69049cd7ec4337
Rod Vagg <rod@vagg.org>
2019-11-01 21:29:31 +1100

    don't unset env so `docker exec` gets the same treatment

--- centos7-devtoolset7/custom_scl_enable.sh
+++ centos7-devtoolset7/custom_scl_enable.sh
@@ -1,5 +1,5 @@
 # extends /opt/app-root/etc/scl_enable and adds ccache
 
-unset BASH_ENV PROMPT_COMMAND ENV
+# unset BASH_ENV PROMPT_COMMAND ENV
 source scl_source enable devtoolset-7
 export PATH=/usr/lib64/ccache:$PATH
